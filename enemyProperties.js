574be7458ec24d7152637af52baab4f31f3b3d5f
Hoyt Jolly <hoytbiz@gmail.com>
2024-11-14 12:31:23 -0500

    Create enemyProperties.js

---
+++ enemyProperties.js
@@ -0,0 +1,148 @@
+
+        // Enemies
+        this.enemyProperties = {
+            smallPumpkin: {
+                size: 12,
+                tall: 24,
+                health: 2,
+                damage: 20,
+                scale: 0.2,
+                speed: 0.5
+            },
+            birdMummy: {
+                size: 10,
+                tall: 50,
+                health: 5,
+                damage: 30,
+                scale: 0.2,
+                speed: 1.5
+            },
+            blackBat: {
+                size: 12,
+                tall: 20,
+                health: 1,
+                damage: 20,
+                scale: 0.2,
+                speed: 2.8
+            },
+            largePumpkin: {
+                size: 25,
+                tall: 40,
+                health: 10,
+                damage: 20,
+                scale: 0.3,
+                speed: 0.3
+            },
+            smallSkeleton: {
+                size: 12,
+                tall: 70,
+                health: 30,
+                damage: 30,
+                scale: 0.25,
+                speed: 0.5
+            },
+            greenMonster: {
+                size: 15,
+                tall: 60,
+                health: 80,
+                damage: 40,
+                scale: 0.28,
+                speed: 0.1
+            },
+            largeGhost: {
+                size: 25,
+                tall: 80,
+                health: 60,
+                damage: 20,
+                scale: 0.35,
+                speed: 2.3
+            },
+            largeSkeleton: {
+                size: 30,
+                tall: 90,
+                health: 30,
+                damage: 60,
+                scale: 0.4,
+                speed: 0.8
+            },
+            witch: {
+                size: 18,
+                tall: 80,
+                health: 40,
+                damage: 30,
+                scale: 0.32,
+                speed: 0.2
+            },
+            wolfEnemy: {
+                size: 15,
+                tall: 70,
+                health: 20,
+                damage: 50,
+                scale: 0.25,
+                speed: 3.5
+            },
+            mummy: {
+                size: 40,
+                tall: 80,
+                health: 40,
+                damage: 80,
+                scale: 0.30,
+                speed: 4
+            },
+            smallBoss: {
+                size: 50,
+                tall: 90,
+                health: 80,
+                damage: 80,
+                scale: 0.45,
+                speed: 0.1
+            },
+            largeBoss: {
+                size: 60,
+                tall: 100,
+                health: 200,
+                damage: 120,
+                scale: 0.6,
+                speed: 0.1
+            },
+            vineTiny: {
+                size: 8,
+                tall: 20,
+                health: 3,
+                damage: 10,
+                scale: 0.5,
+                speed: 2.5
+            },
+            vineSmall: {
+                size: 12,
+                tall: 30,
+                health: 6,
+                damage: 16,
+                scale: 0.4,
+                speed: 1.5
+            },
+            vineBig: {
+                size: 18,
+                tall: 30,
+                health: 12,
+                damage: 30,
+                scale: 0.4,
+                speed: 0.5
+            },
+            vineHuge: {
+                size: 25,
+                tall: 30,
+                health: 30,
+                damage: 50,
+                scale: 0.8,
+                speed: 1.8
+            },
+            vineGiant: {
+                size: 35,
+                tall: 30,
+                health: 50,
+                damage: 80,
+                scale: 1,
+                speed: 0.8
+            }
+        };
