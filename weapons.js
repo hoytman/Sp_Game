9c5a5ccf125178ea35f358f5a46cf8373663531d
Hoyt Jolly <hoytbiz@gmail.com>
2024-11-14 12:30:09 -0500

    Create weapons.js

---
+++ weapons.js
@@ -0,0 +1,138 @@
+this.weapons = {
+            jellyBeans: {
+                fire_rate: 180, // 1/8 of a second in ms
+                power: 1,
+                range: 300,
+                speed: 10,
+                size: 3,
+                spread: 10,
+                nextShotAt: 0
+            },
+            chocolateShield: {
+                fire_rate: 100, // 1/8 of a second in ms
+                power: 1,
+                range: 100,
+                radius: 120,
+                speed: 0,
+                turnSpeed: 10, // large number is slower
+                size: 20,
+                spread: 10,
+                nextShotAt: 0,
+                hitDelay: 300,
+                nextHitAt: 0,
+                scale: 0.2,
+                spin: 0
+            },
+            lollipop: {
+                fire_rate: 180, // 1/8 of a second in ms
+                power: 1,
+                range: 800,
+                speed: 5,
+                size: 3,
+                spread: 30,
+                scale: 0.15,
+                nextShotAt: 0,
+                hitDelay: 20,
+                nextHitAt: 0,
+                spin: .2
+            },
+            candyCorn: {
+                fire_rate: 1000, // 1/5 of a second in ms
+                power: 1,
+                range: 1500,
+                speed: 1,
+                size: 15,
+                spread: 30,
+                scale: 0.3,
+                nextShotAt: 0,
+                spin: .03
+            },
+            cottonCandy: {
+                fire_rate: 2000, // 1/8 of a second in ms
+                power: 1,
+                range: 1800,
+                speed: 2,
+                size: 40,
+                spread: 0,
+                scale: 0.3,
+                nextShotAt: 0,
+                hitDelay: 300,
+                nextHitAt: 0,
+                spin: .01
+            },
+            candyBag: {
+                fire_rate: 2000, // 1/8 of a second in ms
+                power: 0,
+                range: 1800,
+                speed: 2,
+                size: 50,
+                spread: 0,
+                scale: 0.15,
+                nextShotAt: 0,
+                candyRate: 1,
+                hitDelay: 5,
+                nextHitAt: 0,
+            },
+            iceCream: {
+                fire_rate: 3000, // 1/8 of a second in ms
+                power: 1,
+                range: 5000,
+                speed: 0,
+                size: 40,
+                spread: 0,
+                scale: 0.3,
+                nextShotAt: 0,
+                hitDelay: 5,
+                nextHitAt: 0,
+                generationMax: 3,
+            },
+            peppermint: {
+                fire_rate: 500, // 1/8 of a second in ms
+                power: 1,
+                range: 1800,
+                speed: 0,
+                size: 20,
+                spread: 0,
+                scale: 0.2,
+                nextShotAt: 0,
+                hitDelay: 300,
+                nextHitAt: 0,
+                spin: 1.5
+            },
+            cake: {
+                fire_rate: 2000, // 1/8 of a second in ms
+                power: 1,
+                range: 1800,
+                speed: 2,
+                size: 50,
+                spread: 0,
+                scale: 0.3,
+                nextShotAt: 0,
+                hitDelay: 5,
+                nextHitAt: 0,
+            },
+            bonbon: {
+                fire_rate: 800, // 1/8 of a second in ms
+                power: 2,
+                range: 3000,
+                speed: 5,
+                size: 10,
+                spread: 0,
+                scale: 0.1,
+                nextShotAt: 0,
+                hitDelay: 5,
+                nextHitAt: 0,
+                spin: .1
+            },
+            cupCake: {
+                fire_rate: 700, // 1/5 of a second in ms
+                power: 1,
+                range: 1500,
+                speed: 1,
+                size: 15,
+                spread: 30,
+                scale: 0.15,
+                nextShotAt: 0,
+                spin: .1,
+            }
+        };
