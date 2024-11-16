fdb5a763633767e8f47deb7db9387ff26335769f
Hoyt Jolly <hoytbiz@gmail.com>
2024-11-16 00:08:41 -0500

    Update timeline.js

--- timeline.js
+++ timeline.js
@@ -7,230 +7,230 @@
             // then cont with large and small p 
             // bla
             [
-              -1, 'func|createStars'
+              -1, "func|createStars"
             ],
             [
-              -1, 'func|slideStars'
+              -1, "func|slideStars"
             ],
             [
-              -1, 'func|fadeStars'
+              -1, "func|fadeStars"
             ],
             [
-                0, 'smallPumpkin|circle|r:20|s:150'
+                0, "smallPumpkin|circle|r:20|s:150"
             ],
             [
-                20, 'smallPumpkin|circle|r:0.02',
+                20, "smallPumpkin|circle|r:0.02"
             ],
             [
-                0, 'smallPumpkin|circle|r:20'
+                0, "smallPumpkin|circle|r:20"
             ],
             [
-                15, 'smallPumpkin|circle|r:0.015'
+                15, "smallPumpkin|circle|r:0.015"
             ],
             [
-                0, 'largePumpkin|circle|r:10|s:20'
+                0, "largePumpkin|circle|r:10|s:20"
             ],
             [
                 30,
-                'smallPumpkin|circle|r:0.02',
-                'largePumpkin|circle|r:0.01'
+                "smallPumpkin|circle|r:0.02",
+                "largePumpkin|circle|r:0.01"
             ],
             // Attack with a group of bats
             // Then add bats to the mis
             [
-                0, 'blackBat|angle|r:6|s:100'
+                0, "blackBat|angle|r:6|s:100"
 
             ],
             [
                 30,
-                'smallPumpkin|circle|r:0.02',
-                'largePumpkin|circle|r:0.02',
-                'blackBat|circle|r:0.005'
+                "smallPumpkin|circle|r:0.02",
+                "largePumpkin|circle|r:0.02",
+                "blackBat|circle|r:0.005"
             ],
             // Attack with a group of birdMummy
             // Then add birdMummy to the mis
             [
-                0, 'birdMummy|angle|r:6|s:100',
-                'func|playBoneMusic'
+                0, "birdMummy|angle|r:6|s:100",
+                "func|playBoneMusic"
             ],
             [
                 30,
-                'smallPumpkin|circle|r:0.02',
-                'largePumpkin|circle|r:0.01',
-                'blackBat|circle|r:0.005',
-                'birdMummy|circle|r:0.02|'
+                "smallPumpkin|circle|r:0.02",
+                "largePumpkin|circle|r:0.01",
+                "blackBat|circle|r:0.005",
+                "birdMummy|circle|r:0.02|"
             ],
             // Attack with a group of birdMummy and bats
             // Then add birdMummy to the mis
             [
-                0, 'birdMummy|angle|r:6|s:100',
-                'blackBat|angle|r:6|s:100'
+                0, "birdMummy|angle|r:6|s:100",
+                "blackBat|angle|r:6|s:100"
 
             ],
             [
                 20,
-                'smallPumpkin|circle|r:0.02',
-                'largePumpkin|circle|r:0.01',
+                "smallPumpkin|circle|r:0.02",
+                "largePumpkin|circle|r:0.01",
             ],
             // No pumpkins, just skel.
             [
                 40,
-                'birdMummy|circle|r:0.03',
-                'smallSkeleton|circle|r:0.01',
+                "birdMummy|circle|r:0.03",
+                "smallSkeleton|circle|r:0.01",
             ],
             // BAt Attack!
             [
-                0, 'blackBat|angle|r:6|s:100'
+                0, "blackBat|angle|r:6|s:100"
 
             ],
             [
-                0, 'blackBat|angle|r:6|s:100'
+                0, "blackBat|angle|r:6|s:100"
 
             ],
             [
-                0, 'blackBat|angle|r:6|s:100'
+                0, "blackBat|angle|r:6|s:100"
 
             ],
             [
                 40,
-                'birdMummy|circle|r:0.03',
-                'smallSkeleton|circle|r:0.01',
+                "birdMummy|circle|r:0.03",
+                "smallSkeleton|circle|r:0.01",
             ],
             // Monster!
             [
-                0, 'greenMonster|circle|r:20',
-                'func|playMadnessMusic'
+                0, "greenMonster|circle|r:20",
+                "func|playMadnessMusic"
             ],
             [
                 20,
-                'birdMummy|circle|r:0.03',
-                'smallSkeleton|circle|r:0.01',
+                "birdMummy|circle|r:0.03",
+                "smallSkeleton|circle|r:0.01",
             ],
             [
                 40,
-                'birdMummy|circle|r:0.03',
-                'smallSkeleton|circle|r:0.01',
-                'blackBat|circle|r:0.01|s:100'
+                "birdMummy|circle|r:0.03",
+                "smallSkeleton|circle|r:0.01",
+                "blackBat|circle|r:0.01|s:100"
             ],
             [
-                0, 'largeGhost|angle|r:5|s:50'
+                0, "largeGhost|angle|r:5|s:50"
             ],
             [
                 40,
-                'birdMummy|circle|r:0.01',
-                'smallSkeleton|circle|r:0.01',
-                'blackBat|circle|r:0.01',
-                'smallPumpkin|circle|r:0.02',
-                'largePumpkin|circle|r:0.01',
+                "birdMummy|circle|r:0.01",
+                "smallSkeleton|circle|r:0.01",
+                "blackBat|circle|r:0.01",
+                "smallPumpkin|circle|r:0.02",
+                "largePumpkin|circle|r:0.01",
             ],
             [
                 25,
-                'smallPumpkin|circle|r:0.02',
-                'largePumpkin|circle|r:0.02',
-                'largeSkeleton|circle|r:0.01',
+                "smallPumpkin|circle|r:0.02",
+                "largePumpkin|circle|r:0.02",
+                "largeSkeleton|circle|r:0.01",
             ],
             [
-                0, 'whitch|angle|r:5|s:30',
-                'func|playEasyLevelMusic'
+                0, "witch|angle|r:5|s:30",
+                "func|playEasyLevelMusic"
             ],
             [
                 35,
-                'smallPumpkin|circle|r:0.02',
-                'largePumpkin|circle|r:0.02',
-                'largeSkeleton|circle|r:0.01',
+                "smallPumpkin|circle|r:0.02",
+                "largePumpkin|circle|r:0.02",
+                "largeSkeleton|circle|r:0.01",
             ],
             [
-                0, 'largeGhost|circle|r:20',
-                'greenMonster|circle|r:20'
+                0, "largeGhost|circle|r:20",
+                "greenMonster|circle|r:20"
             ],
             [
                 20,
-                'birdMummy|circle|r:0.02',
-                'largePumpkin|circle|r:0.02',
+                "birdMummy|circle|r:0.02",
+                "largePumpkin|circle|r:0.02",
             ],
             [
                 25,
-                'largeSkeleton|circle|r:0.01',
-                'whitch|circle|r:0.01',
+                "largeSkeleton|circle|r:0.01",
+                "witch|circle|r:0.01",
             ],
             [
-                'largeSkeleton|circle|r:0.01',
-                'whitch|circle|r:0.01',
+                "largeSkeleton|circle|r:0.01",
+                "witch|circle|r:0.01",
             ],
             [
                 40,
-                'birdMummy|circle|r:0.01',
-                'smallSkeleton|circle|r:0.01',
-                'blackBat|circle|r:0.01',
-                'smallPumpkin|circle|r:0.03',
-                'largePumpkin|circle|r:0.01',
-                'greenMonster|circle|r:0.01'
+                "birdMummy|circle|r:0.01",
+                "smallSkeleton|circle|r:0.01",
+                "blackBat|circle|r:0.01",
+                "smallPumpkin|circle|r:0.03",
+                "largePumpkin|circle|r:0.01",
+                "greenMonster|circle|r:0.01"
             ],
             [
                 20,
-                'birdMummy|circle|r:0.01',
-                'smallSkeleton|circle|r:0.01',
-                'blackBat|circle|r:0.01',
+                "birdMummy|circle|r:0.01",
+                "smallSkeleton|circle|r:0.01",
+                "blackBat|circle|r:0.01",
             ],
             [
-                0,'func|playRunMusic'
+                0,"func|playRunMusic"
             ],
             [
-                30, 'wolfEnemy|circle|r:0.01'
+                30, "wolfEnemy|circle|r:0.01"
             ],
             [
-                20, 'whitch|circle|r:0.03',
+                20, "witch|circle|r:0.03",
             ],
             [
-                0, 'greenMonster|angle|r:40|s:50'
-            ],
-            [
-                20
-            ],
-            [
-                30, 'mummy|circle|r:0.01'
+                0, "greenMonster|angle|r:40|s:50"
             ],
             [
                 20
             ],
             [
-                0, 'vineTiny|circle|r:20',
-                'func|playBossMusic'
+                30, "mummy|circle|r:0.01"
             ],
             [
-                20, 'vineTiny|circle|r:0.03'
+                20
             ],
             [
-                0, 'largeBoss|angle|r:1'
+                0, "vineTiny|circle|r:20",
+                "func|playBossMusic"
             ],
             [
-                10, 'vineTiny|circle|r:0.03'
+                20, "vineTiny|circle|r:0.03"
             ],
             [
-                0, 'vineSmall|angle|r:40|s:50'
+                0, "largeBoss|angle|r:1"
             ],
             [
-                10, 'vineTiny|circle|r:0.03',
-                'vineSmall|circle|r:0.03',
-                'vineBig|circle|r:0.03',
+                10, "vineTiny|circle|r:0.03"
             ],
             [
-                0, 'vineHuge|circle|r:40'
+                0, "vineSmall|angle|r:40|s:50"
+            ],
+            [
+                10, "vineTiny|circle|r:0.03",
+                "vineSmall|circle|r:0.03",
+                "vineBig|circle|r:0.03",
+            ],
+            [
+                0, "vineHuge|circle|r:40"
             ],
             [
                 10
             ],
             [
-                0, 'vineGiant|circle|r:40'
+                0, "vineGiant|circle|r:40"
             ],
             [
                 10
             ],
             [
-                10, 'vineTiny|circle|r:0.02',
-                'vineSmall|circle|r:0.02',
-                'vineBig|circle|r:0.02',
-                'vineHuge|circle|r:0.005',
-                'vineGiant|circle|r:0.002'
+                10, "vineTiny|circle|r:0.02",
+                "vineSmall|circle|r:0.02",
+                "vineBig|circle|r:0.02",
+                "vineHuge|circle|r:0.005",
+                "vineGiant|circle|r:0.002"
             ]
         ];
